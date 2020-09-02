93184faa89f1f1addc20fd188e7d04c48eca6c08
catcarbonell <hello@catcarbn.com>
2020-09-01 22:45:30 -0700

    forgot to change the hover color for the buttons lol whoops

--- src/Components/Login/SignIn.js
+++ src/Components/Login/SignIn.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SignIn = (props) => {
     return (
         <form className="lg:w-350 w-full bg-white rounded-lg shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <h2 className="text-2xl font-bold mb-4">Sign In</h2>
+            <h2 className="text-2xl font-bold mb-4 uppercase">Sign In</h2>
             <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
                 Username
@@ -19,11 +19,11 @@ const SignIn = (props) => {
             <p className="text-red-500 text-xs italic">Please enter a password.</p>
             </div>
         <div className="flex flex-col items-center justify-between">
-          <button className="bg-custom-limegreen hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          <button className="bg-custom-limegreen hover:bg-green-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
             Sign In
           </button>
           <br />
-          <p className="text-sm mt-4 text-blue-500 hover:text-blue-800 cursor-pointer">
+          <p className="text-sm mt-4 text-green-500 hover:text-green-600 cursor-pointer">
             Forgot Password?
           </p>
         </div>
--- src/Components/Login/SignUp.js
+++ src/Components/Login/SignUp.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SignUp = (props) => {
     return (       
         <form className="md:w-350 w-full bg-white shadow-md rounded-lg px-8 pt-6 pb-8">
-            <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
+            <h2 className="uppercase text-2xl font-bold mb-4">Sign Up</h2>
             <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
                     Create your Username
@@ -32,7 +32,7 @@ const SignUp = (props) => {
             </div>
 
             <div className="flex flex-col items-center justify-between">
-                <button className="bg-custom-limegreen hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                <button className="bg-custom-limegreen hover:bg-green-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                     Sign Up
                 </button>
             </div>
