48f7bcfa7ffd3936d671257fe8986960fd2563d1
Gavi Schneider <gavischneider@gmail.com>
2020-09-02 20:12:45 +0300

    Added shopping cart component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
-import Landing from './Components/Landing';
-import Login from './Components/Login';
-import './tailwind.output.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Landing from "./Components/Landing";
+import Login from "./Components/Login";
+import "./tailwind.output.css";
+import ShoppingCart from "./Components/ShoppingCart";
 
 function App() {
   return (
     <div className="App min-h-screen">
-       
       <div className="flex flex-col">
         <Navbar />
-          <main className="flex-1">
-            <Switch>
-              <Route exact path="/" component={Landing} />
-              <Route path="/login" component={Login} />
-            </Switch>
-          </main>
+        <main className="flex-1">
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route path="/shoppingCart" component={ShoppingCart} />
+          </Switch>
+        </main>
         <Footer />
       </div>
-
     </div>
-
   );
 }
 
