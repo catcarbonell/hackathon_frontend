c466d646578bc9d8614a7a3f792895c1577cc1b6
catcarbonell <hello@catcarbn.com>
2020-10-02 16:59:57 -0700

    fixed height of search bar section on landing

--- src/Components/Landing/Four.js
+++ src/Components/Landing/Four.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Four = () => {
     return(
-        <div id="search" className="text-center overflow-hidden h-screen my-2 flex flex-col justify-center content-center items-center m-auto">
+        <div id="search" className="text-center overflow-hidden py-20 md:py-40 my-2 flex flex-col justify-center content-center items-center m-auto">
             <h3 className="text-4xl font-bold text-gray-100 mb-10">Find what you're looking for</h3>
            
                 <input type="text" className="lg:w-1/2 w-5/6 px-8 py-4 mb-4 rounded-lg shadow-md" placeholder="Search for local businesses or specific goods!" value="" />
